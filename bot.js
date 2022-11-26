18cb35c64881b8008273f6b8d7eb5195cfe1458e
Amiro <amir1387hossein1387@gmail.com>
2022-11-26 21:14:21 +0330

    first commit

---
+++ bot.js
@@ -0,0 +1,47 @@
+var Telegram=require('node-telegram-bot-api')
+var bot=new Telegram('5696293826:AAGNv2K-XTrFxNkmjbB9EnMr46g27dxbU3w',{polling:true})
+
+
+
+bot.onText(/\/start/ , msg=>{
+    bot.sendMessage(
+        msg.chat.id ,
+            'سلام  ' +
+        msg.chat.first_name +
+            ' به ربات من خوش آمدی ',
+            {
+                reply_markup : {
+                    'keyboard' : [
+                        ['WebSite','My Project'],
+                        ['Send Me Message']
+                    ]
+                }
+            }
+        )
+    }
+)
+bot.on('message',async (msg) =>
+    {
+        console.log(msg.text);
+        
+        switch(msg.text){
+            case 'WebSite' :
+                bot.sendMessage(
+                    msg.chat.id , 'WebSite : Aho-amiro.ir'
+                )
+            break;
+
+            case 'My Project' :
+                bot.sendMessage(
+                    msg.chat.id , 'ChatAPP : chat.aho-amiro.ir'
+                )
+            break;
+            
+            case 'Send Me Message' :
+                bot.sendMessage(
+                    msg.chat.id , 'Send Your Message'
+                )
+            break;
+        }
+    }
+)
